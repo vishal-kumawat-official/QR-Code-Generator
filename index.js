e2232dce65f8348a619da52f4b8defcd0f3da297
vishal <vishal.official208@gmail.com>
2022-07-03 15:28:47 +0530

    added Enter key funtion

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var generateButton = document.querySelector(".form button");
 var qrInput = document.querySelector(".form input");
 var qrImg = document.querySelector(".qr-code img");
 
-generateButton.addEventListener("click" , ()=>{
+function buttonPressed(){
     let qrValue = qrInput.value;
     if(!qrValue){
         alert("ERROR: Enter URl or Text!!");
@@ -18,11 +18,24 @@ generateButton.addEventListener("click" , ()=>{
         generateButton.innerText = "Generate QR Code"
         wrapper.classList.add("active");
     })
+}
+
+//button clicked
+generateButton.addEventListener("click" , ()=>{
+    buttonPressed();
 });
 
+//input when empty
 qrInput.addEventListener("keyup",()=>{
     if(!qrInput.value){
         wrapper.classList.remove("active");
+    }
+});
+
+//Enter key pressed to submit
+document.addEventListener("keydown",function(event){
+    if(event.key === "Enter"){
+        buttonPressed();
     }
 });
 
